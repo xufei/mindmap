c4c4111198d0f736906ddc2c3dbd5d4d557a680b
xufei <xu.fei@outlook.com>
2014-12-12 18:02:54 +0800

    demo

--- scripts/demo/animate.js
+++ scripts/demo/animate.js
@@ -1,29 +1,49 @@
-angular.module("ng-charts").controller("AnimateController", ["$scope", "$timeout", function ($scope, $timeout) {
-    $scope.items = [];
+angular.module("ng-charts", []).controller("AnimateController", ["$scope", "$timeout", function ($scope, $timeout) {
+    $scope.items1 = [];
+    $scope.items2 = [];
 
     var counter = 0;
 
-    function addItem() {
-        var newItem = Math.ceil(100 * (1 + Math.sin((counter++) * Math.PI / 180)));
+    var start1 = 160;
+    var start2 = 180;
 
-        if ($scope.items.length > 500) {
-            $scope.items.splice(0, 1);
+    function addItem() {
+        var newItem1 = start1 + 10 * Math.random() - 5;
+        var newItem2 = start2 + 12 * Math.random() - 6;
+
+        start1 = newItem1;
+        start2 = newItem2;
+
+        if ($scope.items1.length > 500) {
+            $scope.items1.splice(0, 1);
+            $scope.items2.splice(0, 1);
         }
 
-        $scope.items.push(newItem);
+        $scope.items1.push(newItem1);
+        $scope.items2.push(newItem2);
 
         $timeout(function () {
             addItem();
-        }, 10);
+        }, 5);
     }
 
     addItem();
 
-    $scope.path = function() {
+    $scope.path1 = function() {
         var path = "";
 
-        for (var i=0; i<$scope.items.length; i++) {
-            path += 2*i + "," + $scope.items[i] + " ";
+        for (var i=0; i<$scope.items1.length; i++) {
+            path += i + "," + $scope.items1[i] + " ";
+        }
+
+        return path;
+    };
+
+    $scope.path2 = function() {
+        var path = "";
+
+        for (var i=0; i<$scope.items2.length; i++) {
+            path += i + "," + $scope.items2[i] + " ";
         }
 
         return path;
