8485ad3caa796bb4a53aedb71e4191e23e6e83dc
xufei <xu.fei@outlook.com>
2015-01-04 20:35:54 +0800

    asdsadasda

---
+++ scripts/controllers/simple/radar.js
@@ -0,0 +1,16 @@
+angular.module("ng-charts").controller("RadarController", ["$scope", "Chart", function ($scope, Chart) {
+    var chart = new Chart();
+
+    $scope.chart = chart;
+
+    $scope.$watch("data", function(data) {
+        chart.data = data;
+        chart.numbers = [];
+
+        for (var i=0; i<data.length; i++) {
+            chart.numbers[i] = data[i].value;
+        }
+
+        chart.scale1();
+    }, true);
+}]);
--- scripts/model/chart.js
+++ scripts/model/chart.js
@@ -108,6 +108,109 @@ angular.module("ng-charts").factory("Chart", [function () {
             }
             
             this.xAxisPath = "M" + this.offsetLeft + ",0 L" + (this.offsetLeft + this.width) + ",0";
+        },
+        scale1: function () {
+            this.clear();
+
+            // 先取用对数取最大最小数据的数量级，如果大于1，就使用非线性坐标轴，如果小于等于1，就用线性坐标轴
+            var max = Math.max.apply(null, this.numbers);
+            var min = Math.min.apply(null, this.numbers);
+
+            if (max === min) {
+                if (min === 0) {
+                    max = 1;
+                } else if (min < 0) {
+                    max = 0;
+                } else {
+                    min = 0;
+                }
+            }
+
+            // 不管哪种坐标轴，都需要在最大最小值的基础上找等分点
+            var range = max - min;
+            var oom = Math.floor(Math.log(range) / Math.LN10);
+            var step = Math.pow(10, oom);
+
+            if (oom > 2) {
+                min = Math.pow(10, Math.floor(Math.log(min) / Math.LN10));
+                max = Math.pow(10, Math.ceil(Math.log(max) / Math.LN10));
+
+                for (var i=min; i<=max; i*=10) {
+                    this.yAxis.push({
+                        number: i
+                    });
+                }
+            }
+            else {
+                min = 0;
+                max = max+step;
+
+                for (var i=min; i<max; i+=step) {
+                    this.yAxis.push({
+                        number: i
+                    });
+                }
+            }
+
+            var stepLength;
+
+            if (oom > 2) {
+                stepLength = this.height / (Math.log(this.yAxis[this.yAxis.length-1].number) * Math.LOG10E);
+            }
+            else {
+                stepLength = this.height / this.yAxis[this.yAxis.length-1].number;
+            }
+
+            // 横向的刻度线
+            for (var i=0; i<this.yAxis.length; i++) {
+                var y;
+
+                if (oom > 2) {
+                    y = Math.log(this.yAxis[i].number) * Math.LOG10E * stepLength;
+                }
+                else {
+                    y = this.yAxis[i].number * stepLength;
+                }
+
+                this.yAxis[i].y = y;
+
+                var total = this.numbers.length;
+                var pathArr = [];
+
+                for (var j=0; j<this.numbers.length; j++) {
+                    var x = Math.round(Math.cos((j/total) * (2 * Math.PI)) * 50 * i) + 300;
+                    var y = Math.round(Math.sin((j/total) * (2 * Math.PI)) * 50 * i) + 300;
+
+                    pathArr.push(x + " " + y + " ");
+                }
+
+                pathArr[pathArr.length] = pathArr[0];
+
+                this.yAxis[i].path = "M " + pathArr.join("L ");
+                console.log(this.yAxis[i].path);
+            }
+
+            // 数据
+            for (var i=0; i<this.data.length; i++) {
+                var x = i * 50;
+                var height;
+
+                if (oom > 2) {
+                    height = Math.log(this.numbers[i]) * Math.LOG10E * stepLength;
+                }
+                else {
+                    height = this.numbers[i] * stepLength;
+                }
+
+                this.xAxis.push({
+                    number: this.numbers[i],
+                    x: x,
+                    height: height
+                });
+                this.xAxis[i].path = "M" + x + ",0 L" + x + ", 0";
+            }
+
+            this.xAxisPath = "M" + this.offsetLeft + ",0 L" + (this.offsetLeft + this.width) + ",0";
         }
     };
 
